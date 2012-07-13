568b0de287c3460498c4695404b19e19c7b84b26
Clement Farabet <clement.farabet@gmail.com>
2012-07-13 14:13:46 -0700

    Added useful shortcuts: ls/cd

--- init.lua
+++ init.lua
@@ -1,5 +1,25 @@
+-- C lib
 require 'libfs'
+
+-- import all symbols
 fs = {}
 for key,val in pairs(libfs) do
    fs[key] = val
 end
+
+-- create convenient shortcuts
+fs['ls'] = function(dir)
+              local list = fs.readdir(dir or '.')
+              for _,elt in ipairs(list) do
+                 print(elt)
+              end
+              return list
+           end
+fs['cd'] = function(dir)
+              local ok = fs.chdir(dir)
+              if not ok then
+                 print('cd: could not change directory to: ' .. dir)
+              else
+                 print('cwd: ' .. fs.cwd())
+              end
+           end
